c7034b7e3394ec43bd853b02b4530e711292973c
rebekahmw <rebekah.warburton@boxmodel.com>
2022-09-01 07:33:04 +0100

    feat: imported WeatherForecast into dashboard - API connected

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from "next";
 import style from "../../utils/styles";
 import dashboard from "../../styles/dashboard/Dashboard.module.scss";
+import WeatherForecast from "../../modules/weather/weather";
 
 const Dashboard: NextPage = () => {
 	return (
 		<div className={style([dashboard.container])}>
             <h1>Hello</h1>
 			<div className={style([dashboard.data])}>
-				<div className={style([dashboard.sideColumn, dashboard.left, dashboard.mobile])}>column</div>
+				<div className={style([dashboard.sideColumn, dashboard.left, dashboard.mobile])}>
+					<WeatherForecast/>
+				</div>
 				<div className={style([dashboard.middleInfo, dashboard.mobile])}>middle stuff</div>
 				<div className={style([dashboard.sideColumn, dashboard.right])}>column</div>
 			</div>
