ae433ba7c45f0016c088efc07e66df0eadb615f6
rebekahmw <rebekah.warburton@boxmodel.com>
2022-09-01 07:21:38 +0100

    feat: added QueryClientProvider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { config } from '@fortawesome/fontawesome-svg-core'
   import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
+export const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
